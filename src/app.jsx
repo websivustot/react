c896c914c5cd70e1ea1ce40db6aba79efc4863e1
websivustot <websivustot@gmail.com>
2017-07-10 13:56:47 +0300

    beginning environment

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+    <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi beatae commodi consequatur ducimus earum eos esse impedit, iusto labore libero maiores molestias nam odio optio, porro praesentium quaerat quis tempora?</p>
+    </div>,
+app);
